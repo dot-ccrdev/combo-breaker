4699e0f6d5b0287ad1d6077378df32730f6bdf78
cid <carlcidromero@gmail.com>
2023-04-25 22:51:18 -0700

    Refactor: Combine start and pause buttons

--- src/components/timer/index.js
+++ src/components/timer/index.js
@@ -1,28 +1,39 @@
 import { h } from "preact";
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 
 const initialSeconds = 180;
+const initialStartPauseLabel = "Start";
 let intervalId;
 
 const Timer = () => {
   const [running, setRunning] = useState(false);
   const [seconds, setSeconds] = useState(initialSeconds);
+  const [startPauseLabel, setStartPauseLabel] = useState(
+    initialStartPauseLabel
+  );
 
-  const handlePause = (event) => {
+  useEffect(() => {
+    if (running) {
+      setStartPauseLabel("Pause");
+      return;
+    }
+    setStartPauseLabel(initialStartPauseLabel);
+  }, [running]);
+
+  const toggleRunningTimer = (event) => {
     event.preventDefault();
+    if (!running) {
+      setRunning(true);
+      intervalId = setInterval(() => {
+        setSeconds((seconds) => seconds - 1);
+      }, 1000);
+      return;
+    }
     clearInterval(intervalId);
     setRunning(false);
   };
 
-  const handleStart = (event) => {
-    event.preventDefault();
-    setRunning(true);
-    intervalId = setInterval(() => {
-      setSeconds((seconds) => seconds - 1);
-    }, 1000);
-  };
-
-  const handleReset = (event) => {
+  const resetTimer = (event) => {
     event.preventDefault();
     clearInterval(intervalId);
     intervalId = undefined;
@@ -42,19 +53,12 @@ const Timer = () => {
     >
       <div>{seconds}</div>
       <div style={{ display: "flex", gap: "1rem" }}>
-        <button style={{ maxWidth: "4rem" }} onClick={handleStart}>
-          Start
+        <button style={{ maxWidth: "4rem" }} onClick={toggleRunningTimer}>
+          {startPauseLabel}
         </button>
         <button
           style={{ maxWidth: "4rem" }}
-          onClick={handlePause}
-          disabled={!running}
-        >
-          Pause
-        </button>
-        <button
-          style={{ maxWidth: "4rem" }}
-          onClick={handleReset}
+          onClick={resetTimer}
           disabled={running === false && seconds === initialSeconds}
         >
           Reset
