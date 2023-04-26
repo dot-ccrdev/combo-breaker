e2e28ef0835b6a385a01f8a358d81d6df92291f9
cid <carlcidromero@gmail.com>
2023-04-25 22:35:21 -0700

    Feat: Disable reset button dynamically

--- src/components/round/index.js
+++ src/components/round/index.js
@@ -3,7 +3,17 @@ import Timer from "../timer";
 
 const Round = () => {
   return (
-    <div>
+    <div
+      style={{
+        border: "1px solid black",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "1rem",
+        padding: "1rem",
+      }}
+    >
+      <b>Round 1</b>
       <Timer />
     </div>
   );
--- src/components/timer/index.js
+++ src/components/timer/index.js
@@ -5,15 +5,18 @@ const initialSeconds = 180;
 let intervalId;
 
 const Timer = () => {
+  const [running, setRunning] = useState(false);
   const [seconds, setSeconds] = useState(initialSeconds);
 
   const handlePause = (event) => {
     event.preventDefault();
     clearInterval(intervalId);
+    setRunning(false);
   };
 
   const handleStart = (event) => {
     event.preventDefault();
+    setRunning(true);
     intervalId = setInterval(() => {
       setSeconds((seconds) => seconds - 1);
     }, 1000);
@@ -23,6 +26,7 @@ const Timer = () => {
     event.preventDefault();
     clearInterval(intervalId);
     intervalId = undefined;
+    setRunning(false);
     setSeconds(initialSeconds);
   };
 
@@ -44,11 +48,15 @@ const Timer = () => {
         <button
           style={{ maxWidth: "4rem" }}
           onClick={handlePause}
-          disabled={!intervalId}
+          disabled={!running}
         >
           Pause
         </button>
-        <button style={{ maxWidth: "4rem" }} onClick={handleReset}>
+        <button
+          style={{ maxWidth: "4rem" }}
+          onClick={handleReset}
+          disabled={running === false && seconds === initialSeconds}
+        >
           Reset
         </button>
       </div>
